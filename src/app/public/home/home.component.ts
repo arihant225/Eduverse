8879fe8c09fff391cd4b7f44397061c9e59a3cb9
Arihant jain <jarihant225@gmail.com>
2023-06-04 00:21:52 +0530

    added counter to ui

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public TutorsCount:number=0;
+  public TuteesCount:number=0;
+  public InstituteCount:number=0;
+
+  ngOnInit(): void {
+    let tutorLimit=200;
+    let tuteesLimit=700;
+    let instituteLimit=360;
+    
+    
+    let Timer=setInterval(() => {
+      if(tutorLimit>this.TutorsCount)
+      this.TutorsCount++;
+      if(tuteesLimit>this.TuteesCount)
+      this.TuteesCount++;
+      if(instituteLimit>this.InstituteCount)
+      this.InstituteCount++;
+      if(tuteesLimit==this.TuteesCount && tutorLimit== this.TutorsCount && instituteLimit == this.InstituteCount )
+      {
+        clearInterval(Timer)
+        clearTimeout(Timer)
+      }
+      
+    }, 1);
+  }
   public educationGenres: string[] = [
     "STEM (Science, Technology, Engineering, and Mathematics)",
     "Humanities",
@@ -18,4 +44,7 @@ export class HomeComponent {
     "Special Education",
     "Financial Education"
   ];
+
+
+
 }
