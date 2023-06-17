2c63f2ed59d43ddee384ce792c33f89f365cda97
Arihant jain <jarihant225@gmail.com>
2023-06-18 01:50:11 +0530

    adding sign up prompt

---
+++ src/app/Functionality/Prompt.ts
@@ -0,0 +1,145 @@
+import { FormControl, Validators } from "@angular/forms";
+import { Subject, interval } from "rxjs";
+
+export class UserPrompt {
+    public values: string[] = [];
+    public currentIndex: number = -1;
+    public currentPrompt: Subject<string> = new Subject<string>();
+    public errorPrompt: Subject<string|null> = new Subject<string|null>();
+    public currentPromptIntervalRef:any;
+    public currentErrorPromptInterval:any;
+
+    constructor() {
+        for (let prompt of this.prompts) {
+            this.values.push("");
+        }
+
+    }
+    public prompts: string[] = [
+        "Sure, let's get started with your registration process. I'll guide you through the necessary steps to complete your registration.",
+        "Please provide your full name in the format of [First Name] [Last Name].",
+        "Kindly enter the email address you would like to use for your registration.",
+        "An OTP (One-Time Password) has been sent to the email address you provided. Please check your inbox and enter the OTP here to verify your email.",
+        "Please enter your phone number, including the country code if applicable.",
+        "Choose a password for your account. Please ensure it meets the required criteria, such as having a minimum length, containing both letters and numbers, etc.",
+        "To ensure there are no typos, please re-enter your chosen password for confirmation.",
+        "If you have a referral code, please let us know. A referral code is a unique identifier that may provide you with certain benefits or bonuses.",
+        "Please indicate whether you are signing up as an individual (self) or on behalf of an institution (organization, company, etc.)."
+    ];
+    public controls: FormControl[] = [
+        new FormControl('', []),
+        new FormControl('', [Validators.required, Validators.pattern(/^.*\s.*$/)]),
+        new FormControl('', [Validators.required, Validators.email]),
+        new FormControl('', [Validators.required, Validators.pattern(/^\d{6}$/)]),
+        new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
+        new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/)]),
+        new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/)]),
+        new FormControl('', []),
+        new FormControl('', [])
+    ];
+
+    public errorMessages: string[] = [
+        "",
+        "Please enter your full name.",
+        "Please enter a valid email address.",
+        "Please enter a valid OTP (6-digit number).",
+        "Please enter a valid phone number (10-digit number).",
+        "Please enter a valid password (minimum 8 characters, at least one letter, one number, and one special character).",
+        "Please confirm your password.",
+        "",
+        ""
+    ];
+
+    public checkError() {
+        if (this.controls[this.currentIndex].errors) {
+            this.typeError();
+            return true;
+        }
+        else
+            return false;
+    }
+    public next() {
+        if(this.currentErrorPromptInterval)
+        {
+            clearInterval(this.currentErrorPromptInterval)
+        }
+        if(this.currentPromptIntervalRef)
+        {
+            clearInterval(this.currentPromptIntervalRef)
+        }
+        this.currentPrompt.complete();
+        this.currentPrompt=new Subject<string>();
+        this.errorPrompt.complete();
+        this.errorPrompt=new Subject<string|null>();
+ 
+        this.currentPrompt=new Subject<string>();
+        if (this.currentIndex >= this.prompts.length - 1) {
+            return;
+        }
+
+        if (this.currentIndex >= 0 && this.checkError())
+            return;
+            this.errorPrompt.next(null)
+       
+        this.currentIndex++;
+        this.typePrompt();
+    }
+    public prev() {
+        
+        if(this.currentErrorPromptInterval)
+        {
+            clearInterval(this.currentErrorPromptInterval)
+        }
+        if(this.currentPromptIntervalRef)
+        {
+            clearInterval(this.currentPromptIntervalRef)
+        }
+        this.currentPrompt.complete();
+        this.currentPrompt=new Subject<string>();
+        if (this.currentIndex <= 0) {
+            return;
+        }
+        this.currentIndex--;
+        this.typePrompt();
+    }
+    save(val: string) {
+
+        this.values[this.currentIndex] = val;
+
+    }
+
+    typePrompt() {
+       
+        let prompt = this.prompts[this.currentIndex];
+        let tempPrompt = "";
+        let tempIndex = 0;
+        this.currentPromptIntervalRef = setInterval(() => {
+            if (tempIndex >= prompt.length - 1) {
+                clearInterval(this.currentPromptIntervalRef);
+                
+            }
+            tempPrompt = tempPrompt + prompt[tempIndex++];
+            this.currentPrompt.next(tempPrompt);
+
+
+
+        }, 50)
+    }
+    typeError() {
+        let prompt = this.errorMessages[this.currentIndex];
+        let tempPrompt = "<br><br><br>";
+        let tempIndex = 0;
+        this.currentErrorPromptInterval = setInterval(() => {
+            if (tempIndex >= prompt.length - 1) {
+                clearInterval(this.currentErrorPromptInterval);
+               
+            }
+            tempPrompt = tempPrompt + prompt[tempIndex++];
+            this.errorPrompt.next(tempPrompt);
+        }, 50)
+
+
+    }
+
+
+}
--- src/app/public/signup/signup.component.ts
+++ src/app/public/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserPrompt } from 'src/app/Functionality/Prompt';
 import { IFormAttributes } from 'src/app/Interfaces/IFormAttributes';
 
 @Component({
@@ -8,93 +9,46 @@ import { IFormAttributes } from 'src/app/Interfaces/IFormAttributes';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  public FormAttributes:IFormAttributes[]=[
-    {
-      placeholder: "First Name",
-      inputType: "text",
-      icon: "bi-person",
-      formControlName: "firstName",
-      required: true,
-      maxLength: 50,
-    },
-    {
-      placeholder: "Last Name",
-      inputType: "text",
-      icon: "bi-person",
-      formControlName: "lastName",
-      required: true,
-      maxLength: 50,
-    },
-    {
-      placeholder: "Email Address",
-      inputType: "email",
-      icon: "bi-envelope",
-      formControlName: "email",
-      required: true,
-      maxLength: 100,
-    },
-    {
-      placeholder: "Phone Number",
-      inputType: "tel",
-      icon: "bi-telephone",
-      formControlName: "phoneNumber",
-      required: false,
-      maxLength: 20,
-    },
-    {
-      placeholder: "Password",
-      inputType: "password",
-      icon: "bi-lock",
-      formControlName: "password",
-      required: true,
-      maxLength: 50,
-    },
-    {
-      placeholder: "Confirm Password",
-      inputType: "password",
-      icon: "bi-lock",
-      formControlName: "confirmPassword",
-      required: true,
-      maxLength: 50,
-    },
-    {
-      placeholder: "Eduverse Institute Code",
-      inputType: "text",
-      icon: "bi-building",
-      formControlName: "eduverseInstituteCode",
-      required: false,
-      maxLength: 20,
-    },
-    {
-      placeholder: "Role/Position",
-      inputType: "text",
-      icon: "bi-briefcase",
-      formControlName: "role",
-      required: false,
-      maxLength: 100,
-    },
-  ];
-  /**
-   *
-   */
+  public prompt="";
+  public errorPrompt:string|null=null;
+  public signUpPrompts:UserPrompt=new UserPrompt();
+  public promptInput=new FormControl("",[]);
   constructor(private formBuilder:FormBuilder) {
-        
+ 
   }
-  public signupForm:FormGroup=this.formBuilder.group({})
-  ngOnInit(): void {
-    this.signupForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.maxLength(50)]],
-      lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
-      phoneNumber: ['', [Validators.maxLength(20)]],
-      password: ['', [Validators.required, Validators.maxLength(50)]],
-      confirmPassword: ['', [Validators.required, Validators.maxLength(50),this.passwordCheck]],
-      eduverseInstituteCode: ['', [Validators.maxLength(20)]],
-      role: ['', [Validators.maxLength(100)]],
-    });
-  }
-  private passwordCheck(password:FormControl){
+  
+  ngOnInit(): void { 
+   
+    this.signUpPrompts.next();
+  
+    this.signUpPrompts.currentPrompt.subscribe(d=>{
+      this.prompt=d;
+    })
 
+
+  }
+  public next(){
+    this.signUpPrompts.next();
+      this.promptInput=this.signUpPrompts.controls[this.signUpPrompts.currentIndex];
+      this.signUpPrompts.currentPrompt.subscribe(d=>{
+        this.prompt=d;
+      })
+      this.signUpPrompts.errorPrompt.subscribe(d=>{
+        this.errorPrompt=d;
+      })
+   
+
+  }
+  public back(){
+    this.signUpPrompts.prev()
+    this.promptInput=this.signUpPrompts.controls[this.signUpPrompts.currentIndex];
+   
+    this.signUpPrompts.currentPrompt.subscribe(d=>{
+      this.prompt=d;
+    })
+  }
+
+  private passwordCheck(password:FormControl){
   }
   }
 
